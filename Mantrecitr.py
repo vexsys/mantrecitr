964eb8d9a46fde331f2605534a5e7f212e0a5bba
vexsys <82122159+vexsys@users.noreply.github.com>
2021-04-07 14:37:29 -0700

    Add files via upload
    Added the ability to change the delay between recitations.

--- Mantrecitr.py
+++ Mantrecitr.py
@@ -2,16 +2,21 @@ import time
 
 mantra = "No Mantra Entered"
 reps = 0
+timedelay = 0
 count = 1
 
 print('Enter desired Mantra')
 mantra = input()
 print('Enter Number of Repetitions')
 reps = int(input())
+print("Enter Time Delay (1 = 1 second)")
+timedelay = float(input())
 
 while reps > 0:
     print(count,":", mantra)
     reps -= 1
     count += 1
+    time.sleep(timedelay)
     
 print("00((0)0)(0)")
+input("Press ENTER to Exit")
